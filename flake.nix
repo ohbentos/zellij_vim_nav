cad041e5246ea8498ef8d8c7b69f1bd214806b3c
ohbentos <mateusbentoa@gmail.com>
2026-02-03 11:47:00 -0300

    fix(nix): add required openssl and pkg-config packages

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "Zellij vim-nav plugin for seamless vim/pane navigation";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay = {
@@ -8,24 +7,32 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
     let
-      supportedSystems = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" "aarch64-linux" ];
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      packages = forAllSystems (system:
+      packages = forAllSystems (
+        system:
         let
           pkgs = import nixpkgs {
             inherit system;
             overlays = [ rust-overlay.overlays.default ];
           };
-
           rustToolchain = pkgs.rust-bin.stable.latest.default.override {
             targets = [ "wasm32-wasip1" ];
           };
-
           rustPlatform = pkgs.makeRustPlatform {
             cargo = rustToolchain;
             rustc = rustToolchain;
@@ -36,11 +43,17 @@
             pname = "zellij-vim-nav";
             version = "0.1.0";
             src = ./.;
-
             cargoHash = "sha256-fqT+fOgigotqEiv84u2Y1TsCILr47M/KyJKWSveZ060=";
 
+            # Add OpenSSL for native build dependencies
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl ];
+
+            # https://www.tweag.io/blog/2022-09-22-rust-nix/
             buildPhase = ''
+              runHook preBuild
               cargo build --release --target wasm32-wasip1
+              runHook postBuild
             '';
 
             installPhase = ''
